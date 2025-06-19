3d7b4e20274abd1435939f3adb7dcc8170913534
speexx87 <speexx87@users.noreply.github.com>
2025-06-19 19:21:17 +0000

    Update main page to include LotteryHistory component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,36 @@
-import Header from "../components/Header";
-import LotteryCard from "../components/LotteryCard";
-import Table from "../components/Table";
-import style from "../styles/Home.module.css";
+import React from 'react';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Header from '../components/Header';
+import LotteryCard from '../components/LotteryCard';
+import LotteryHistory from '../components/LotteryHistory';
+import { LotteryProvider } from '../context/context';
 
-const Home = () => {
+const Home: React.FC = () => {
   return (
-    <div className={style.wrapper}>
-      <Header />
-      <LotteryCard />
-      <Table />
-    </div>
+    <LotteryProvider>
+      <div className={styles.container}>
+        <Head>
+          <title>Decentralized Lottery</title>
+          <meta name="description" content="Decentralized Lottery Platform" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <Header />
+        
+        <main className={styles.main}>
+          <div className={styles.gridContainer}>
+            <div className={styles.lotterySection}>
+              <LotteryCard />
+            </div>
+            
+            <div className={styles.historySection}>
+              <LotteryHistory />
+            </div>
+          </div>
+        </main>
+      </div>
+    </LotteryProvider>
   );
 };
 
